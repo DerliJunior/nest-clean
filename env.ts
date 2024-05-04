35bb14e68ff82516ed21c75e6d48c1a6bfd1a8fb
DerliJunior <contact.derli@gmail.com>
2024-05-04 00:26:29 -0300

    Ajustes nas configurações do projeto

--- env.ts
+++ env.ts
@@ -1,6 +1,9 @@
 import { z } from "zod";
 
 export const envSchema = z.object({
+  JWT_PRIVATE_KEY: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
+  JWT_HS256: z.string(),
   APP_PORT: z.coerce.number().optional().default(3333),
 });
 
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get<ConfigService<Env, true>>(ConfigService);
-  const port = configService.get("APP_PORT");
+  const port = configService.get<number>("APP_PORT");
 
   await app.listen(port);
 }
