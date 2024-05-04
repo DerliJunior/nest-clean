47acbb1b190544cfafec5035167be8a2a63b0990
DerliJunior <contact.derli@gmail.com>
2024-05-04 00:28:14 -0300

    Adicionando os módulos para o Nest encontra-los

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,39 @@ import { PrismaService } from "./prisma/prisma.service";
 import { CreateAccountController } from "./controllers/create-account.controller";
 import { ConfigModule } from "@nestjs/config";
 import { envSchema } from "env";
+import { UserService } from "./core/user/user.service";
+import { UserRepository } from "./core/user/user-repository";
+import { QuestionService } from "./core/question/question.service";
+import { QuestionRepository } from "./core/question/question-repository";
+import { QuestionController } from "./controllers/question.controller";
+import { AuthModule } from "./auth/auth.module";
+import { AutenticationController } from "./controllers/autentication.controller";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./auth/jwt.strategy";
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    validate: (env) =>  envSchema.parse(env),
-    isGlobal: true
-  })],
-  controllers: [AppController, CreateAccountController],
-  providers: [AppService, PrismaService],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    JwtModule,
+    AuthModule,
+  ],
+  controllers: [
+    AppController,
+    CreateAccountController,
+    QuestionController,
+    AutenticationController,
+  ],
+  providers: [
+    AppService,
+    PrismaService,
+    UserService,
+    UserRepository,
+    QuestionService,
+    QuestionRepository,
+    JwtStrategy,
+  ],
 })
 export class AppModule {}
